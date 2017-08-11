0f08c8c1ca6a39bc168a9b55376973f46d13ee3d
jerry <83814616@qq.com>
2017-08-11 15:59:02 +0800

    add

--- src/src/main/java/com/taimeitech/pass/workflowExt/Listener/TraceCountListener.java
+++ src/src/main/java/com/taimeitech/pass/workflowExt/Listener/TraceCountListener.java
@@ -2,18 +2,21 @@ package com.taimeitech.pass.workflowExt.Listener;
 
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
-import org.springframework.stereotype.Component;
 
+/**
+ * Created by yanjie.miao on 2017/8/11.
+ */
+public class TraceCountListener  implements TaskListener {
 
-public class FinanceUserListener implements TaskListener {
     private static final long serialVersionUID = 1L;
 
     @Override
     public void notify(DelegateTask delegateTask) {
         String approved = (String) delegateTask.getVariable("approved");
         if (approved.equals("true")) {
-            Long agreeCounter = (Long) delegateTask.getVariable("approvedFinanceCounter");
-            delegateTask.setVariable("approvedFinanceCounter", agreeCounter + 1);
+            Long agreeCounter = (Long) delegateTask.getVariable("approvedTraceCounter");
+            delegateTask.setVariable("approvedTraceCounter", agreeCounter + 1);
         }
     }
+
 }
